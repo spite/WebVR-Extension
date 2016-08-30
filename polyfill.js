3e764751b7e8716ed3c58597182d1844dcda7167
johnmaf <johns@matterandform.net>
2016-08-30 14:18:59 -0400

    Update animationFrame methods to conform to spec

--- polyfill.js
+++ polyfill.js
@@ -132,7 +132,13 @@ var source = '(' + function () {
 
 	VRDisplay.prototype.requestAnimationFrame = function( c ) {
 
-		requestAnimationFrame( c );
+		return requestAnimationFrame( c );
+
+	}
+
+	VRDisplay.prototype.cancelAnimationFrame = function(handle) {
+
+		cancelAnimationFrame(handle);
 
 	}
 

c8ff05e6ebce79e9f9a07fe98e790faf78607806
Jaume Sanchez <the.spite@gmail.com>
2016-09-18 00:26:15 +0200

    split polyfill from content-script

--- polyfill.js
+++ polyfill.js
@@ -1,68 +1,4 @@
-var verbose = false;
-
-function log() {
-
-	if( !verbose ) return;
-
-	console.log.apply(
-		console, [
-			`%c WebVREmu `,
-			'background: #007AA3; color: #ffffff; text-shadow: 0 -1px #000; padding: 4px 0 4px 0; line-height: 0',
-			...arguments
-		]
-	);
-
-}
-
-log( 'Polyfill', window.location.toString() );
-
-var port = chrome.runtime.connect( { name: 'contentScript' } );
-
-function post( msg ) {
-
-	port.postMessage( msg );
-
-}
-
-post( { method: 'script-ready' } );
-
-port.onMessage.addListener( function( msg ) {
-
-	switch( msg.action ) {
-		case 'pose':
-		var e = new CustomEvent( 'webvr-pose', {
-			detail: {
-				position: msg.position,
-				rotation: msg.rotation
-			}
-		} );
-		window.dispatchEvent( e );
-		break;
-		case 'hmd-activate':
-		var e = new CustomEvent( 'webvr-hmd-activate', {
-			detail: {
-				state: msg.value
-			}
-		} );
-		window.dispatchEvent( e );
-		break;
-	}
-
-} );
-
-window.addEventListener( 'webvr-ready', function() {
-
-	post( { action: 'page-ready' } );
-
-} );
-
-window.addEventListener( 'webvr-resetpose', function() {
-
-	post( { action: 'reset-pose' } );
-
-} );
-
-var source = '(' + function () {
+function injectedScript() {
 
 	'use strict';
 
@@ -476,9 +412,4 @@ var source = '(' + function () {
 	var event = new Event( 'webvr-ready' );
 	window.dispatchEvent( event );
 
-} + ')();';
-
-var script = document.createElement('script');
-script.textContent = source;
-(document.head||document.documentElement).appendChild(script);
-script.parentNode.removeChild(script);
+}
